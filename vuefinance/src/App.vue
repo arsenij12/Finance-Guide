52c1427d78b3fe698ac9bfc25a60894e4d5a285f
arsenij12 <56472584+arsenij12@users.noreply.github.com>
2024-04-17 10:59:41 +0500

    new file:   vuefinance/.gitignore 	new file:   vuefinance/README.md 	new file:   vuefinance/babel.config.js 	new file:   vuefinance/jsconfig.json 	new file:   vuefinance/package-lock.json 	new file:   vuefinance/package.json 	new file:   vuefinance/public/index.html 	new file:   vuefinance/src/App.vue 	new file:   vuefinance/src/main.js 	new file:   vuefinance/vue.config.js

---
+++ vuefinance/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <input v-model="newItemText" @keyup.enter="addItem" placeholder="Введите новый элемент">
+    
+    <ul>
+      <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newItemText: '',
+      items: []
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.newItemText.trim() !== '') {
+        this.items.push(this.newItemText);
+        this.newItemText = ''; 
+      }
+    }
+  }
+};
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+</style>
