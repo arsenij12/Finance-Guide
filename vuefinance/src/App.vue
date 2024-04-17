886688cea2444139eb5f81fd6ef8e6e08e67b6cc
arsenij12 <56472584+arsenij12@users.noreply.github.com>
2024-04-17 20:40:49 +0500

    modified:   vuefinance/package-lock.json 	modified:   vuefinance/package.json 	modified:   vuefinance/public/index.html 	modified:   vuefinance/src/App.vue 	new file:   vuefinance/src/assets/add_circle_black_24dp.svg 	new file:   vuefinance/src/components/Modal.vue 	modified:   vuefinance/src/main.js

--- vuefinance/src/App.vue
+++ vuefinance/src/App.vue
@@ -1,22 +1,60 @@
 <template>
-  <div class="form-container">
-    <div>
-      <input v-model="newTransaction.amount" type="number" placeholder="Сумма">
-      <input v-model="newTransaction.date" type="date" placeholder="Дата">
-      <select v-model="newTransaction.category">
-        <option value="Питание">Питание</option>
-        <option value="Транспорт">Транспорт</option>
-        <option value="Развлечения">Развлечения</option>
-      </select>
-      <button @click="addTransaction">Добавить транзакцию</button>
-    </div>
-      <ul class="transaction-list">
-      <li v-for="(transaction, index) in transactions" :key="index">
-        {{ transaction.amount }} - {{ transaction.date }} - {{ transaction.category }}
-      </li>
-    </ul>
+  <div>
+    <header class="header">
+      <div class="header-container">
+        <v-dialog v-model="dialog" max-width="400" persistent>
+          <template v-slot:activator="{ props: activatorProps }">
+            <v-btn v-bind="activatorProps" class="dialog-btn">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"/></svg>
+              Open Dialog
+            </v-btn>
+          </template>
+
+          <v-card class="dialog-card">
+            <v-card-title class="dialog-title">
+              Введите прибыль за месяц
+            </v-card-title>
+            <v-card-text class="dialog-content">
+              <form>
+                <input type="number" placeholder="Месячная прибыль">
+              </form>
+            </v-card-text>
+            <template v-slot:actions>
+              <div class="dialog-actions">
+                <v-btn @click="dialog = false" class="dialog-btn">
+                  Disagree
+                </v-btn>
+                <v-btn @click="dialog = false" class="dialog-btn">
+                  Agree
+                </v-btn>
+              </div>
+            </template>
+          </v-card>
+        </v-dialog>
+      </div>
+    </header>
+    <main>
+      <div class="form-container">
+        <div>
+          <input v-model="newTransaction.amount" type="number" placeholder="Сумма">
+          <input v-model="newTransaction.date" type="date" placeholder="Дата">
+          <select v-model="newTransaction.category">
+            <option value="Питание">Питание</option>
+            <option value="Транспорт">Транспорт</option>
+            <option value="Развлечения">Развлечения</option>
+          </select>
+          <button @click="addTransaction">Добавить транзакцию</button>
+        </div>
+        <ul class="transaction-list">
+          <li v-for="(transaction, index) in transactions" :key="index">
+            {{ transaction.amount }} - {{ transaction.date }} - {{ transaction.category }}
+          </li>
+        </ul>
+      </div>
+    </main>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -27,7 +65,8 @@ export default {
         date: null,
         category: 'Питание' 
       },
-      transactions: [] 
+      transactions: [],
+      dialog: false
     };
   },
   mounted() {
@@ -58,6 +97,34 @@ export default {
 </script>
 
 <style scoped>
+.dialog-btn {
+  color: #1976d2;
+}
+
+.dialog-content {
+  padding: 20px;
+}
+
+.dialog-actions {
+  display: flex;
+  justify-content: flex-end;
+  padding: 8px;
+}
+
+.dialog-card {
+  max-width: 400px;
+}
+
+.dialog-title {
+  font-size: 20px;
+  font-weight: bold;
+}
+.header{
+  background-color: #0beb2962;
+  color: #fff;
+  padding: 20px;
+  text-align: center;
+}
 .form-container {
   max-width: 400px;
   margin: 0 auto;
@@ -102,5 +169,9 @@ export default {
   padding: 10px;
   margin-bottom: 5px;
   border-radius: 5px;
+}
+
+.icon{
+  color: black;
 }
 </style>
--- vuefinance/src/main.js
+++ vuefinance/src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, fa } from 'vuetify/iconsets/fa4'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    },
+  },
+})
+
+createApp(App).use(vuetify).mount('#app')
